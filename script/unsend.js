8ab2e8e7f7e7a3c94ae453dc7183f02c6122ebcd
syahrulkr7 <168558459+syahrulkr7@users.noreply.github.com>
2024-05-08 23:19:11 +0700

    Update unsend.js

--- script/unsend.js
+++ script/unsend.js
@@ -15,5 +15,5 @@ module.exports.run = async function({
 }) {
   if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage("I can't unsend from other message.", event.threadID, event.messageID);
   if (event.type != "message_reply") return api.sendMessage("Reply to bot message", event.threadID, event.messageID);
-  return api.unsendMessage(event.messageReply.messageID, err => (err) ? api.sendMessage("Something went wrong.", event.threadID, event.messageID) : '');
+  return api.unsendMessage(event.messageReply.messageID, err => (err) ? api.sendMessage("Ada yang salah.", event.threadID, event.messageID) : '');
 }
